bdf20d23b5742856769f15a090f4359d0bd446f1
Justin Gottula <justin@jgottula.com>
2012-10-24 12:58:21 -0700

    register: used reflection to simplify assoc array initialization

--- src/register.d
+++ src/register.d
@@ -5,49 +5,27 @@
  */
 module register;
 
+import std.string;
+
 
 static this() {
-	regNames["al"] = Register.al;
-	regNames["ah"] = Register.ah;
-	regNames["ax"] = Register.ax;
-	regNames["eax"] = Register.eax;
-	
-	regNames["bl"] = Register.bl;
-	regNames["bh"] = Register.bh;
-	regNames["bx"] = Register.bx;
-	regNames["ebx"] = Register.ebx;
-	
-	regNames["cl"] = Register.cl;
-	regNames["ch"] = Register.ch;
-	regNames["cx"] = Register.cx;
-	regNames["ecx"] = Register.ecx;
-	
-	regNames["dl"] = Register.dl;
-	regNames["dh"] = Register.dh;
-	regNames["dx"] = Register.dx;
-	regNames["edx"] = Register.edx;
-	
-	regNames["esi"] = Register.esi;
-	regNames["edi"] = Register.edi;
-	
-	regNames["esp"] = Register.esp;
-	regNames["ebp"] = Register.ebp;
-	
-	regNames["eip"] = Register.eip;
-	regNames["eflags"] = Register.eflags;
+	/* reflection: loop over all enum members */
+	foreach (reg; __traits(allMembers, Register)) {
+		regNames[reg.toLower()] = mixin("Register." ~ reg);
+	}
 }
 
 
 public enum Register {
-	al, ah, ax, eax,
-	bl, bh, bx, ebx,
-	cl, ch, cx, ecx,
-	dl, dh, dx, edx,
+	AL, AH, AX, EAX,
+	BL, BH, BX, EBX,
+	CL, CH, CX, ECX,
+	DL, DH, DX, EDX,
 	
-	esi, edi,
-	esp, ebp,
+	ESI, EDI,
+	ESP, EBP,
 	
-	eip, eflags,
+	EIP, EFLAGS,
 }
 
 public Register[string] regNames;
