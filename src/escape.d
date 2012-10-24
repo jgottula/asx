111bdb57bab2fe9e337b4b5e7be567d6e65d0df4
Justin Gottula <justin@jgottula.com>
2012-10-24 05:15:17 -0700

    escape: added more functions, unit tests

--- src/escape.d
+++ src/escape.d
@@ -6,12 +6,56 @@
 module escape;
 
 
+/* TODO: add support for octal/decimal/hex escape sequences */
+
+
 bool isEscape(char c) {
 	return (c == '\a' || c == '\b' || c == '\f' || c == '\n' || c == '\r' ||
 		c == '\t' || c == '\v');
 }
 
-string escapize(char c) {
+bool isEscapeStr(string s) {
+	assert(s.length >= 2);
+	
+	switch (s[0..2]) {
+	case "\\a":
+	case "\\b":
+	case "\\f":
+	case "\\n":
+	case "\\r":
+	case "\\t":
+	case "\\v":
+		return true;
+	default:
+		return false;
+	}
+}
+
+char escapize(string s) {
+	assert(s.length >= 2);
+	assert(isEscapeStr(s));
+	
+	switch (s[0..2]) {
+	case "\\a":
+		return '\a';
+	case "\\b":
+		return '\b';
+	case "\\f":
+		return '\f';
+	case "\\n":
+		return '\n';
+	case "\\r":
+		return '\r';
+	case "\\t":
+		return '\t';
+	case "\\v":
+		return '\v';
+	default:
+		return '\0';
+	}
+}
+
+string unescapize(char c) {
 	switch (c) {
 	case '\a':
 		return "\\a";
@@ -30,4 +74,34 @@ string escapize(char c) {
 	default:
 		return [c];
 	}
+}
+
+
+unittest {
+	assert(isEscapeStr("\\a"));
+	assert(isEscapeStr("\\b"));
+	assert(!isEscapeStr("\\c"));
+	assert(!isEscapeStr("\\d"));
+	assert(!isEscapeStr("\\e"));
+	assert(isEscapeStr("\\f"));
+	assert(!isEscapeStr("\\g"));
+	assert(!isEscapeStr("\\h"));
+	assert(!isEscapeStr("\\i"));
+	assert(!isEscapeStr("\\j"));
+	assert(!isEscapeStr("\\k"));
+	assert(!isEscapeStr("\\l"));
+	assert(!isEscapeStr("\\m"));
+	assert(isEscapeStr("\\n"));
+	assert(!isEscapeStr("\\o"));
+	assert(!isEscapeStr("\\p"));
+	assert(!isEscapeStr("\\q"));
+	assert(isEscapeStr("\\r"));
+	assert(!isEscapeStr("\\s"));
+	assert(isEscapeStr("\\t"));
+	assert(!isEscapeStr("\\u"));
+	assert(isEscapeStr("\\v"));
+	assert(!isEscapeStr("\\w"));
+	assert(!isEscapeStr("\\x"));
+	assert(!isEscapeStr("\\y"));
+	assert(!isEscapeStr("\\z"));
 }
