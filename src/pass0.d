2f7de4f7b03507e002cc093ac57513acd1c3f5ec
Justin Gottula <justin@jgottula.com>
2012-10-24 00:16:11 -0700

    pass0: improved overall structure; added line comments

--- src/pass0.d
+++ src/pass0.d
@@ -16,10 +16,11 @@ private enum State {
 	DEFAULT,
 	COMMENT_BLOCK,
 	COMMENT_NEST,
+	COMMENT_LINE,
 }
 
 private class Context {
-	this(string src) {
+	this(in string src) {
 		this.src = src;
 		this.dst = new char[0];
 		
@@ -35,16 +36,30 @@ private class Context {
 		return src.length;
 	}
 	
-	string get(ulong count) {
+	char get() {
+		assert(src.length >= 1);
+		return src[0];
+	}
+	string get(in ulong count) {
 		assert(count <= src.length);
 		return src[0..count];
 	}
 	
-	void put(string buf) {
+	void put(in string buf) {
 		dst ~= buf;
 	}
+	void put(in char c) {
+		dst ~= c;
+	}
 	
-	void advance(ulong count = 1) {
+	void echo(in ulong count = 1) {
+		assert(count <= src.length);
+		
+		put(src[0..count]);
+		advance(count);
+	}
+	
+	void advance(in ulong count = 1) {
 		assert(count <= src.length);
 		
 		++col; // todo: line
@@ -64,6 +79,7 @@ private class Context {
 	ulong commentLevel;
 	
 private:
+	
 	string src;
 	char[] dst;
 }
@@ -71,22 +87,19 @@ private:
 Context ctx;
 
 
-private void error(string msg) {
+private void error(in string msg) {
 	stderr.writefln("[pass0|error|%d:%d] %s", ctx.line, ctx.col, msg);
 	exit(1);
 }
 
-private void warn(string msg) {
+private void warn(in string msg) {
 	stderr.writefln("[pass0|warn|%d:%d] %s", ctx.line, ctx.col, msg);
 }
 
 private void putCommentChar() {
-	if (ctx.get(1) == "\n") {
-		ctx.put("\n");
-	} else {
-		ctx.put(" ");
+	if (ctx.get() == '\n') {
+		ctx.echo();
 	}
-	
 	ctx.advance();
 }
 
@@ -98,53 +111,54 @@ void doPass0(ref string src) {
 		case State.DEFAULT:
 			if (ctx.avail() >= 2 && ctx.get(2) == "/*") {
 				ctx.state = State.COMMENT_BLOCK;
-				ctx.put("  ");
 				ctx.advance(2);
-				break;
 			} else if (ctx.avail() >= 2 && ctx.get(2) == "/+") {
 				ctx.state = State.COMMENT_NEST;
 				ctx.commentLevel = 1;
-				ctx.put("  ");
 				ctx.advance(2);
-				break;
+			} else if (ctx.avail() >= 2 && ctx.get(2) == "//") {
+				ctx.state = State.COMMENT_LINE;
+				ctx.advance(2);
 			} else {
-				ctx.put(ctx.get(1));
-				ctx.advance();
-				break;
+				ctx.echo();
 			}
+			break;
 		case State.COMMENT_BLOCK:
 			if (ctx.avail() >= 2 && ctx.get(2) == "*/") {
 				ctx.state = State.DEFAULT;
-				ctx.put("  ");
 				ctx.advance(2);
-				break;
 			} else {
 				putCommentChar();
-				break;
 			}
+			break;
 		case State.COMMENT_NEST:
 			if (ctx.avail() >= 2 && ctx.get(2) == "/+") {
 				++ctx.commentLevel;
-				ctx.put("  ");
 				ctx.advance(2);
-				break;
 			} else if (ctx.avail() >= 2 && ctx.get(2) == "+/") {
 				if (--ctx.commentLevel == 0) {
 					ctx.state = State.DEFAULT;
 				}
-				ctx.put("  ");
 				ctx.advance(2);
-				break;
 			} else {
 				putCommentChar();
-				break;
 			}
+			break;
+		case State.COMMENT_LINE:
+			if (ctx.get() == '\n') {
+				ctx.state = State.DEFAULT;
+				ctx.echo();
+			} else {
+				ctx.advance();
+			}
+			break;
 		}
 	}
 	
 	/* check for bad EOF states */
 	final switch (ctx.state) {
 	case State.DEFAULT:
+	case State.COMMENT_LINE:
 		/* okay */
 		break;
 	case State.COMMENT_BLOCK:
