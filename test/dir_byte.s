a08b6ee2b73086bf58ed8b1ccbab4527ece56ac5
Justin Gottula <justin@jgottula.com>
2012-10-25 05:32:14 -0700

    added more tests

---
+++ test/dir_byte.s
@@ -0,0 +1,15 @@
+//.byte 0
+//.byte -1
+//.byte 1
+//.byte 255
+//.byte 256
+//.byte 0,1
+//.byte 0,-1
+//.byte 0,0
+//.byte 0,[
+//.byte [
+//.byte
+//.byte 0[
+//.byte 0 [
+//.byte 0,1 [
+//.byte 0,1[
